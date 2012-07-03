9e95cd85ca8ad5dedfafc31901765ef67fa72a68
cavis <ryancavis@gmail.com>
2012-07-03 12:54:20 -0500

    init rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Glistro::Application.config.secret_token = '7fcdae904db9df36eb71697b2eb1320bd3eef1e6b416dc251124fb4fe689eec9c26d2eb0e6e7f902dfa8fb3e7f0b8d4d0426187fefb3b039bc89b466ea44904b'
