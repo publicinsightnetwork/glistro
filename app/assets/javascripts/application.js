d86f89e493016fe9842ebd25a6a0daf515f0847f
cavis <ryancavis@gmail.com>
2012-07-04 13:22:10 -0500

    fix foundation includes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,11 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require foundation
+//= require foundation/app
+//= require foundation/jquery.customforms
+//= require foundation/jquery.orbit-1.4.0
+//= require foundation/jquery.placeholder.min
+//= require foundation/jquery.reveal
+//= require foundation/jquery.tooltips
+//= require foundation/modernizr.foundation
 //= require_tree .
